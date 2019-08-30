956bc670526b788df78e83dcb170010a649541e0
Michael Johnson <44432163+MichaelDavidJohnson@users.noreply.github.com>
2019-08-31 00:05:56 +0100

    Update song_title.py
    Added a delay in the loop so the script is not checking for a change in songs every ~15ms and now only checks around ~2 seconds. Also added implementation that checks if VLC media player is not open.

--- song_title.py
+++ song_title.py
@@ -1,37 +1,61 @@
 import win32gui
+import time
 """
 Python script to save song titles currently being played on VLC for OBS and Twitch streaming. Only has support for VLC at
 the moment.
 """
-
+delay = 2
 def stringFinder():
+    applicationChecker = False
     songTitle = "No song is being played!" #Default response.
+    
     vlcStorage = [] #Stores all of the windows that get enumerated.
+    
     def enumWindowsProc(hwnd,lParam):
+    
         g = win32gui.GetWindowText(hwnd)
+    
         vlcStorage.append(g)
+    
     win32gui.EnumWindows(enumWindowsProc,0)
+    
     for title in vlcStorage:
+    
         if "VLC media player" in title:
+            
+            applicationChecker = True
+        
             songTitle = title
+            
             songTitle = songTitle.replace("- VLC media player","") #Formatting the songs to be of the required form.
+            
             songTitle = songTitle.replace("VLC media player","")
+            
             songTitle = songTitle.replace(".mp3","")
+            
             songTitle = songTitle.replace(".flac","")
+            
             if not songTitle.strip():
+            
                 songTitle = "No song is being played!"
+            
             return songTitle
+    
+    if applicationChecker == False:
+        print("VLC media player is not currently open.")
+        
+    
     return songTitle
 
 
 def txtStorage(songName):
-    file = open("Music.txt","w")
-    file.write(songName)
-    file.truncate() #Truncate is used to get rid of the text.
+    with open('Music.txt','w') as file:
+        file.write(songName)
+        
 
     
 def main():
-    print("test")
+    
     input("Press any key to begin searching for songs being played by VLC media player\n")
     initialTitle = stringFinder()
     txtStorage(initialTitle)
@@ -39,11 +63,15 @@ def main():
         while True:
             newTitle = stringFinder()
             if initialTitle == newTitle:
+                time.sleep(delay)
                 pass
             else:
                 print("The song being shown to the stream is : ",newTitle)
                 txtStorage(newTitle)
                 initialTitle = newTitle
+                time.sleep(delay)
+        
+          
                 
         
     except KeyboardInterrupt: #CTRL-C exits the program.
